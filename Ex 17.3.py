6d22d01323e4dee829cfb8767b7570eff7b05e8f
m-blue <m-blue@users.noreply.github.com>
2016-11-17 17:14:05 -0600

    Add files via upload

---
+++ Ex 17.3.py
@@ -0,0 +1,12 @@
+class Point(object):
+
+    def __init__(self,x=0.0,y=0.0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "%s, %s" % (self.x,self.y)
+
+
+some = Point()
+print some
